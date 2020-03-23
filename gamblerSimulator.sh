d98d8bafebcdd9bae2b81a44e633a0442ce230dc
priyankagarad <garadpriyanka1995@gmail.com>
2020-03-23 16:43:08 +0530

    Added:using random function check gambler win or loose bet

--- gamblerSimulator.sh
+++ gamblerSimulator.sh
@@ -1,5 +1,15 @@
 #!/bin/bash -x
+echo "Welcome Gamling Simulator"
+
 #CONSTANT
 stake=100
 bet=1
-echo "Gambling Simulator"
+
+#check gambler win or loss
+bet=$((RANDOM%2))
+if [ $bet -eq 1 ]
+then
+	printf "win"
+else
+	printf "loose"
+fi
