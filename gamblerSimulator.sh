6c8d61a1c817cbfb20017ca97b68238d7bf4f3e3
priyankagarad <garadpriyanka1995@gmail.com>
2020-03-24 16:08:53 +0530

    Added:calculated win day and loss day ,and how much amount win or loss

--- gamblerSimulator.sh
+++ gamblerSimulator.sh
@@ -2,32 +2,49 @@
 echo "Welcome Gamling Simulator"
 
 #CONSTANT
-STAKE=100
-BET=1
+BET=10
 TOTAL_DAYS=20
 
 #VARIABLE
-amount=$STAKE
-day=0
-win_amount=0
-loss_amount=0
+day=1
+total=0
+win_day=0
+loss_day=0
 
-#calculated amount  after 20 day playing
-while [[ $day -lt $TOTAL_DAYS ]]
+#calculated win amount and loss amount after 20 day playing
+while [[ $day -le $TOTAL_DAYS ]]
 do
-	while [[ $amount -gt 50 && $amount -lt 150 ]]
+	stake=100;
+	win_amount=0
+	loss_amount=0
+	while [[ $stake -gt 50 && $stake -lt 150 ]]
 	do
 		random=$((RANDOM%2))
 		if [ $random -eq 1 ]
 		then
-			amount=$(($amount+$BET))
-			win_amount=$(($win_amount+$amount))
+			stake=$(($stake+$BET))
+			win_amount=$(($win_amount+$BET))
 		else
-			amount=$(($amount-$BET))
-			loss_amount=$(($loss_amount+$amount))
+			stake=$(($stake-$BET))
+			loss_amount=$(($loss_amount+$BET))
 		fi
 	done
-	((day++))
+
+#calculated total win amount ,loss amount and day win or loss
+total_win_amount=$(($total_win_amount+$win_amount))
+total_loss_amount=$(($total_loss_amount+$loss_amount))
+
+if [ $total_win_amount -gt $total_loss_amount ]
+then
+	 result1=$(($total_win_amount-$total_loss_amount))
+	 ((win_day++))
+else
+	result1=$(($total_loss_amount-$total_win_amount))
+	((loss_day++)) 
+fi
+((day++))
 done
-echo "win amount$win_amount"
-echo "loss amount $loss_amount"
+
+echo "win days $win_day"
+echo "loss day $loss_day"
+echo "how much loss or win $result1"
